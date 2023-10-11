854e776903f424382366eb8f435370448887c625
abinasa <147621306+abinasa@users.noreply.github.com>
2023-10-11 18:25:32 +0530

    Add files via upload

---
+++ unit1 challenge1.2.py
@@ -0,0 +1,19 @@
+#leap year
+
+"""
+year % 4==0 &
+year % 100= /
+year % 400==0
+
+"""
+def isLeapyear(year):
+  if(year % 4==0 and year % 100!=0)or year % 400==0:
+    return True
+    return False
+
+year=2010
+
+if isLeapyear(year):
+  print("{} is not a leap year.".format(year))
+else:
+  print("{} is not a leap year.".format(year))
